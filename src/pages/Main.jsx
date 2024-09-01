2c494384a24d0037a22b3a2ef89c48e6b4c168de
ketimetr123 <ketimetreveli14@gmail.com>
2024-09-01 15:02:35 +0400

    added local storage on contact page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,23 @@
+import React, { useEffect } from "react";
 const Main = () => {
+  useEffect(() => {
+    localStorage.setItem("name", "keti metreveli");
+    localStorage.setItem("age", 24);
+    localStorage.setItem("device", "{name: 'iphone', model: '13'}");
+  }, []);
+  const deleteAge = () => {
+    localStorage.removeItem("age");
+  };
   return (
     <div className="w-[200px] mx-auto mt-3">
       <h1>SpaceX Missions</h1>
+
+      <h5>
+        {`${localStorage.getItem("name")} ${localStorage.getItem("age")}`}
+      </h5>
+
+      <h5>{localStorage.getItem("device")}</h5>
+      <button onClick={deleteAge}>delete</button>
     </div>
   );
 };
